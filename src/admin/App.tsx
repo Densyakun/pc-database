c374f9ed67b6f64340af18b1b7465972dd036b40
densyakun <densyakun1128@gmail.com>
2023-02-02 17:34:03 +0000

    update ra-data-simple-prisma to 2.1.2

--- src/admin/App.tsx
+++ src/admin/App.tsx
@@ -4,7 +4,7 @@ import { dataProvider } from "ra-data-simple-prisma";
 import { Prisma } from '@prisma/client';
 
 const App = () => (
-  <Admin dataProvider={dataProvider("/api", {})}>
+  <Admin dataProvider={dataProvider("/api")}>
     {Object.keys(Prisma.ModelName).map(model =>
       <Resource name={model} list={ListGuesser} />
     )}
