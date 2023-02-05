37bda5d3fc0cdf3c668eb5fdbf88285bde5ede2f
densyakun <densyakun1128@gmail.com>
2023-02-05 19:26:24 +0000

    move /lib/ directory to /src/

--- src/pages/api/[resource].ts
+++ src/pages/api/[resource].ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { defaultHandler } from "ra-data-simple-prisma"
-import prismaClient from "../../../lib/prisma"
+import prismaClient from "../../lib/prisma"
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   defaultHandler(req, res, prismaClient)
